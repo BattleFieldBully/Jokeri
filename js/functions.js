e7d688300f3cddb18671b980eeb91a062af168e9
akoukkar <n3koan00@students.oamk.fi>
2024-10-07 11:48:52 +0300

    files uploaded

---
+++ js/functions.js
@@ -0,0 +1,33 @@
+        const riviNappi = document.getElementById('RiviNappi');
+        const jokeriRivit = document.getElementById('jokeriRiviLaskuri');
+        const riviLaskuri = document.getElementById('riviLaskuri');
+
+        let rivit = 0; 
+
+        
+        riviNappi.addEventListener('click', () => {
+            JokeriRivi();
+        });
+
+        
+        function JokeriRivi() {
+            const uusiRivi = document.createElement('div'); 
+            uusiRivi.classList.add('row'); 
+
+            
+            for (let i = 0; i < 7; i++) {
+                const uusiLaatikko = document.createElement('div');
+                uusiLaatikko.classList.add('box'); 
+                
+                uusiLaatikko.textContent = Math.floor(Math.random() * 10);
+                
+                uusiRivi.appendChild(uusiLaatikko);
+            }
+
+            
+            jokeriRivit.appendChild(uusiRivi);
+
+            
+            rivit++;
+            riviLaskuri.textContent = rivit;
+        }
